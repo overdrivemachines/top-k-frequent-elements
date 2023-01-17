946eb6d7f10ee76767df201e324e8c39956d30dd
get.dipen@gmail.com <get.dipen@gmail.com>
2023-01-17 10:08:03 -0800

    Solved

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
-# top-k-frequent-elements
+# 347. Top K Frequent Elements
+
+Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+
+
+
+Example 1:
+
+Input: nums = [1,1,1,2,2,3], k = 2
+Output: [1,2]
+
+
+Example 2:
+
+Input: nums = [1], k = 1
+Output: [1]
+
+
+Constraints:
+
+1 <= nums.length <= 105
+-104 <= nums[i] <= 104
+k is in the range [1, the number of unique elements in the array].
+It is guaranteed that the answer is unique.
+
+
+Follow up: Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
+
+
+5 occurs 45 times
+umap[5] = 45 *
+umap[2] = 10
+umap[9] = 44
+umap[1] = 50 *
+umap[3] = 1
+umap[12] = 32
+umap[7] = 90 *
+umap[-1] = 51 *
+
+
+top 4
+
+result[0] = 5 | 45      lowest_index = 0 lowest_value = 45
+result[1] = 2 | 10 **
+result[2] = 9 | 44
+result[3] = 1 | 50
+
+
+can be evicted: result[1]
--- top-k-frequent-elements.cpp
+++ top-k-frequent-elements.cpp
@@ -1,7 +1,78 @@
 #include <iostream>
+#include <unordered_map>
+#include <vector>
 
 using namespace std;
 
+vector<int> topKFrequent(vector<int>& nums, int k) {
+  unordered_map<int, int> umap;
+
+  vector<int> result;
+
+  // number of occurences of each element in the nums vector
+  for (int num : nums) {
+    umap[num]++;
+  }
+
+  // print umap
+  for (auto u : umap) {
+    cout << u.first << ":" << u.second << "\n";
+  }
+
+
+  int i = 0;
+  // build results vector of k elements
+  for (auto it = umap.begin(); i < k; it++, i++) {
+    result.push_back(it->first);
+  }
+
+  // find lowest index and value in results vector
+  int lowest_result_index = 0;
+  int lowest_result_value = umap[result[0]];
+
+  for (long unsigned int i = 0; i < result.size(); i++) {
+    if (umap[result[i]] < lowest_result_value) {
+      lowest_result_index = i;
+      lowest_result_value = umap[result[i]];
+    }
+  }
+
+
+  i = 0;
+  for (auto u : umap) {
+    if (i++ < k) {
+      // do nothing
+    }
+    else {
+      if (u.second > lowest_result_value) {
+        result[lowest_result_index] = u.first;
+        lowest_result_index = 0;
+        lowest_result_value = umap[result[0]];
+        for (long unsigned int i = 0; i < result.size(); i++) {
+          if (umap[result[i]] < lowest_result_value) {
+            lowest_result_index = i;
+            lowest_result_value = umap[result[i]];
+          }
+        }
+      }
+    }
+  }
+  return result;
+}
+
+// Prints the contents of a vector
+void printv(vector<int> nums) {
+  for (auto n : nums) {
+    cout << n << " ";
+  }
+  cout << "\n";
+}
+
 int main(int argc, char const *argv[]) {
+  vector<int> nums = {1, 1, 1, 2, 2, 3, 4, 6, 77, 77, 6, 77, 4, 77};
+  int k = 3;
+
+  printv(topKFrequent(nums, k));
+
   return 0;
 }
