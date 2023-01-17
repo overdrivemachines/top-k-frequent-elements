ebc3eeb08403a578e6633edc4610cb20eadaa65f
get.dipen@gmail.com <get.dipen@gmail.com>
2023-01-17 10:53:54 -0800

    Optimized using multimap

--- top-k-frequent-elements.cpp
+++ top-k-frequent-elements.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <unordered_map>
+#include <map>
 #include <vector>
 
 using namespace std;
 
-vector<int> topKFrequent(vector<int>& nums, int k) {
+// Works but is slow
+vector<int> topKFrequent_slow(vector<int>& nums, int k) {
   unordered_map<int, int> umap;
 
   vector<int> result;
@@ -59,6 +61,64 @@ vector<int> topKFrequent(vector<int>& nums, int k) {
   }
   return result;
 }
+
+
+// Optimized and uses multimap
+vector<int> topKFrequent(vector<int>& nums, int k) {
+  vector<int> result;
+  int count = 0;
+  unordered_map<int, int> umap;
+  multimap<int, int, greater<int>> mmap;
+
+  // umap will have number of occurences of each element in the nums vector
+  for (int num : nums) {
+    umap[num]++;
+  }
+
+  // Now umap contains something like:
+  // umap[77]:4
+  // umap[6]:2
+  // umap[4]:2
+  // umap[3]:1
+  // umap[2]:2
+  // umap[1]:3
+
+  // Print umap
+  // cout << "umap contents:\n";
+  // for (auto u : umap)
+  //   cout << u.first << ":" << u.second << "\n";
+
+
+  // Insert into multimap mmap
+  for (auto u : umap) {
+    mmap.insert({u.second, u.first});
+  }
+
+  // Now mmap contains
+  // 4:77
+  // 3:1
+  // 2:6
+  // 2:4
+  // 2:2
+  // 1:3
+
+  // Print mmap
+  // cout << "mmap contents:\n";
+  // for (auto m : mmap)
+  //   cout << m.first << ":" << m.second << "\n";
+
+
+  // get the first k elements of mmap
+  for (auto m : mmap) {
+    count++;
+    if (count > k)
+      return result;
+    result.push_back(m.second);
+  }
+
+  return result;
+}
+
 
 // Prints the contents of a vector
 void printv(vector<int> nums) {
